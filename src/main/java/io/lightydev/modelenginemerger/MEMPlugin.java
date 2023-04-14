0ab3221e4b4aa395194bb3b89e9181059a0247df
Lighty <denizgtaturk@gmail.com>
2023-04-14 14:09:58 +0300

    Added bulk merging.

--- src/main/java/io/lightydev/modelenginemerger/MEMPlugin.java
+++ src/main/java/io/lightydev/modelenginemerger/MEMPlugin.java
@@ -5,15 +5,20 @@ import lombok.Getter;
 import lombok.SneakyThrows;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public final class MEMPlugin extends JavaPlugin {
 
     @Getter private static MEMPlugin instance;
-    @Getter private static File modelEngineFolder;
-    @Getter private static File itemsAdderFolder;
+    @Getter private static List<File> modelEngineFolders;
+    @Getter private static List<File> itemsAdderFolders;
 
     @SneakyThrows
     @Override
@@ -24,15 +29,19 @@ public final class MEMPlugin extends JavaPlugin {
 
         Bukkit.getConsoleSender().sendMessage("§a[ModelEngineMerger] Enabling plugin...");
 
-        // Check if config.yml has valid values
-        if (this.getConfig().getString("paths.from") == null || this.getConfig().getString("paths.to") == null) {
-            this.getLogger().severe("Please set the ModelEngine and ItemsAdder folders in the config.yml!");
-            this.getServer().getPluginManager().disablePlugin(this);
+        // Check and convert old config format
+        if (convertOldConfig()) {
+            this.getLogger().info("Old config format detected and successfully converted to the new format.");
         }
 
-        // Setting the paths
-        modelEngineFolder = new File(getDataFolder().getParentFile().toString() + "/" + getConfig().getString("paths.from"));
-        itemsAdderFolder = new File(getDataFolder().getParentFile().toString() + "/" + getConfig().getString("paths.to"));
+        // Ensure the paths list exists in the config.yml
+        if (this.getConfig().get("paths") == null || !(this.getConfig().get("paths") instanceof List<?>)) {
+            this.getLogger().severe("Please add a 'paths' section with folder pairs in the config.yml!");
+            this.getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        loadFolderPairs();
 
         // Register the command
         this.getCommand("mem").setExecutor(new MEMCommand());
@@ -53,9 +62,52 @@ public final class MEMPlugin extends JavaPlugin {
 
     public void reload() {
         this.reloadConfig();
+        loadFolderPairs();
+    }
 
-        modelEngineFolder = new File(getDataFolder().getParentFile().toString() + "/" + getConfig().getString("paths.from"));
-        itemsAdderFolder = new File(getDataFolder().getParentFile().toString() + "/" + getConfig().getString("paths.to"));
+    private void loadFolderPairs() {
+        modelEngineFolders = new ArrayList<>();
+        itemsAdderFolders = new ArrayList<>();
+        List<Map<?, ?>> paths = this.getConfig().getMapList("paths");
+
+        if (paths != null && !paths.isEmpty()) {
+            for (Map<?, ?> path : paths) {
+                String from = (String) path.get("from");
+                String to = (String) path.get("to");
+                modelEngineFolders.add(new File(getDataFolder().getParentFile().toString() + "/" + from));
+                itemsAdderFolders.add(new File(getDataFolder().getParentFile().toString() + "/" + to));
+            }
+        } else {
+            this.getLogger().warning("The 'paths' section is missing or empty in the config.yml!");
+        }
+    }
+
+    private boolean convertOldConfig() {
+        if (this.getConfig().getString("paths.from") != null && this.getConfig().getString("paths.to") != null) {
+            // Get the old paths.from and paths.to values
+            String oldFrom = this.getConfig().getString("paths.from");
+            String oldTo = this.getConfig().getString("paths.to");
+
+            // Remove the old paths.from and paths.to keys
+            this.getConfig().set("paths.from", null);
+            this.getConfig().set("paths.to", null);
+
+            // Create the new paths list with the old folder pair
+            List<Map<String, String>> newPathList = new ArrayList<>();
+            Map<String, String> folderPair = new HashMap<>();
+            folderPair.put("from", oldFrom);
+            folderPair.put("to", oldTo);
+            newPathList.add(folderPair);
+
+            // Set the new paths list in the config.yml
+            this.getConfig().set("paths", newPathList);
+
+            // Save the updated config.yml
+            this.saveConfig();
+
+            return true;
+        }
+        return false;
     }
 
 }
--- src/main/java/io/lightydev/modelenginemerger/MergeManager.java
+++ src/main/java/io/lightydev/modelenginemerger/MergeManager.java
@@ -1,32 +1,61 @@
 package io.lightydev.modelenginemerger;
 
+import lombok.Getter;
+import org.bukkit.Bukkit;
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MergeManager {
 
-    public static boolean merge() throws IOException {
-        AtomicBoolean success = new AtomicBoolean(true);
-        Path modelEngine = MEMPlugin.getModelEngineFolder().toPath();
-        Path itemsAdder = MEMPlugin.getItemsAdderFolder().toPath();
+    @Getter private static final MergeManager instance = new MergeManager();
 
-        Files.walk(modelEngine)
-            .forEach(sourceFile -> {
-                try {
-                    // Compute the destination file path
-                    Path destinationFile = itemsAdder.resolve(modelEngine.relativize(sourceFile));
+    public void mergeFolders() {
+        MEMPlugin plugin = MEMPlugin.getInstance();
+        List<File> modelEngineFolders = plugin.getModelEngineFolders();
+        List<File> itemsAdderFolders = plugin.getItemsAdderFolders();
 
-                    // Copy the file from the source to the destination
-                    Files.copy(sourceFile, destinationFile, StandardCopyOption.REPLACE_EXISTING);
-                    success.set(true);
-                } catch (IOException e) {
-                    // Handle the exception
-                    success.set(false);
-                }
-            });
-        return success.get();
+        for (int i = 0; i < modelEngineFolders.size(); i++) {
+            File sourceFolder = modelEngineFolders.get(i);
+            File destinationFolder = itemsAdderFolders.get(i);
+
+            if (!sourceFolder.exists()) {
+                Bukkit.getConsoleSender().sendMessage("§c[ModelEngineMerger] Source folder doesn't exist: " + sourceFolder.getPath());
+                continue;
+            }
+
+            if (!destinationFolder.exists()) {
+                destinationFolder.mkdirs();
+            }
+
+            try {
+                copyFolder(sourceFolder, destinationFolder);
+                Bukkit.getConsoleSender().sendMessage("§a[ModelEngineMerger] Successfully merged folders: " + sourceFolder.getPath() + " -> " + destinationFolder.getPath());
+            } catch (IOException e) {
+                Bukkit.getConsoleSender().sendMessage("§c[ModelEngineMerger] Failed to merge folders: " + sourceFolder.getPath() + " -> " + destinationFolder.getPath());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void copyFolder(File sourceFolder, File destinationFolder) throws IOException {
+        if (sourceFolder.isDirectory()) {
+            if (!destinationFolder.exists()) {
+                destinationFolder.mkdirs();
+            }
+
+            for (String file : sourceFolder.list()) {
+                File srcFile = new File(sourceFolder, file);
+                File destFile = new File(destinationFolder, file);
+                copyFolder(srcFile, destFile);
+            }
+        } else {
+            Files.copy(sourceFolder.toPath(), destinationFolder.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        }
     }
 }
